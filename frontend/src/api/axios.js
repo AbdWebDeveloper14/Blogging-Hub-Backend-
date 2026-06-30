1ac43530f7b6bef49a82b9dcb53146401ea07486
Nehakhan0 <163759002+Nehakhan0@users.noreply.github.com>
2026-06-30 15:01:27 +0500

    changing backend URI

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://blogging-hub-backend-64477fylk-pixelperfectdesigns-projects.vercel.app', 
+  baseURL: 'https://blogging-hub-backend.vercel.app/api', 
 });
 
 export default API;
